b28b5e497df0a6842f94fb496f7daed123b2c9a1
Allison Bradley <allison.bradley@outlook.com>
2021-02-04 13:08:44 -0700

    Added the writeFile function

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const writeCommits = (data) => { 
-    console.log(data)
+// This is the built-in package for writing to a text file using fs.writeFile.  Takes in three arguments: destination, data, and the error if unsuccessful
+const fs = require('fs')
+
+
+const getData = () => { }
+
+
+const writeCommits = (data) => {
+    fs.writeFile('./data/commits.txt', data, (err) => {
+        if (err) {
+            throw err
+        } else {
+            console.log("Successfully printed to commits.txt")
+            }
+    })
 }
 
-writeCommits("testing")
+writeCommits("hello")
